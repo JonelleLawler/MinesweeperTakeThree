e2d7a4346509e5717a0fdcc38d9b1e210855d658
JonelleLawler <lawlerjonelle@gmail.com>
2021-11-23 14:45:25 +0000

    tile testing

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,19 @@
+import com.tsi.lawler.jonelle.minesweeper.Tile;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class TileTest {
+
+    @Test
+    public void TestTileType()
+    {
+        Tile testTile = new Tile();
+        testTile.SetType(1);
+
+        int testValue = testTile.GetType();
+
+        assertEquals(testValue, 1);
+    }
+
+}
