77b54df0f648ba1d703c04f2eae35960deb5e41f
JonelleLawler <lawlerjonelle@gmail.com>
2021-11-23 14:24:21 +0000

    Changed how tile types are stored

--- src/main/java/com/tsi/lawler/jonelle/minesweeper/Board.java
+++ src/main/java/com/tsi/lawler/jonelle/minesweeper/Board.java
@@ -2,7 +2,7 @@ package com.tsi.lawler.jonelle.minesweeper;
 
 public class Board {
     // Variables
-    private int[][] coords;
+    private Tile[][] coords;
     private boolean[][] board;
     private int height;
     private int width;
@@ -18,21 +18,22 @@ public class Board {
         this.height = h;
         this.width = w;
         this.bombs = b;
-        this.coords = new int[0][0];
+        this.coords = new Tile[this.height][this.width];
         this.board = new boolean[this.height][this.width];
         this.bombCount = 0; // 0 whenever game resets
+        InitTileArr();
     }
 
-  /*  public void InitTiles()
+    public void InitTileArr()
     {
-        for(int i = 0; i < this.width; i++)
+        for(int i = 0; i < this.height; i++)
         {
-            for(int j = 0; i < this.height; j++)
+            for(int j = 0; j < this.width; j++)
             {
-                tileArray[i][j] = new Tile();
+                coords[i][j] = new Tile();
             }
         }
-    } */
+    }
 
     // Set up the board
 
@@ -47,21 +48,21 @@ public class Board {
                if(randomNum == 7)
                 {
                     // set tile type and output
-               //     tileArray[r][c].SetType(1);
+                   this.coords[r][c].SetType(1);
                     System.out.print(" * ");
                 }
                // BOMB = TRUE
                 else if(randomNum == 9)
                 {
                     // set tile type and output
-                   // tileArray[r][c].SetType(1);
+                   this.coords[r][c].SetType(1);
                     System.out.print(" * ");
                 }
                 // BOMB = FALSE
                 else
                 {
                     // set tile type and output
-                  //  tileArray[r][c].SetType(2);
+                    this.coords[r][c].SetType(2);
                     System.out.print(" X ");
                 }
             }
